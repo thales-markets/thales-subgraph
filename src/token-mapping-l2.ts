381d5b034c58733769462c1b4ac22a711716b8bb
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-02-03 15:35:13 +0100

    Add StakedOnBehalf event

--- src/token-mapping-l2.ts
+++ src/token-mapping-l2.ts
@@ -1,10 +1,12 @@
 import { Claim as MigratedRewardsClaimEvent } from '../generated/OngoingAirdrop/OngoingAirdrop';
-import { TokenTransaction } from '../generated/schema';
+import { Staker, TokenTransaction } from '../generated/schema';
 import {
   Staked as StakedEvent,
   Withdrawn as WithdrawnEvent,
   RewardPaid as RewardPaidEvent,
 } from '../generated/LPStakingRewards/LPStakingRewards';
+import { StakedOnBehalf as StakedOnBehalfEvent } from '../generated/StakingThales/StakingThales';
+import { BigInt } from '@graphprotocol/graph-ts';
 
 export function handleMigratedRewardsClaimEvent(event: MigratedRewardsClaimEvent): void {
   let tokenTransaction = new TokenTransaction(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
@@ -48,4 +50,21 @@ export function handleRewardPaidEvent(event: RewardPaidEvent): void {
   tokenTransaction.type = 'lpClaimStakingRewards';
   tokenTransaction.blockNumber = event.block.number;
   tokenTransaction.save();
+}
+
+export function handleStakedOnBehalfEvent(event: StakedOnBehalfEvent): void {
+  let staker = Staker.load(event.params.staker.toHex());
+  if (staker === null) {
+    staker = new Staker(event.params.staker.toHex());
+    staker.account = event.params.staker;
+    staker.stakedAmount = event.params.amount;
+    staker.escrowedAmount = BigInt.fromI32(0);
+    staker.totalStakedAmount = event.params.amount;
+    staker.unstakingAmount = BigInt.fromI32(0);
+  } else {
+    staker.stakedAmount = staker.stakedAmount.plus(event.params.amount);
+    staker.totalStakedAmount = staker.stakedAmount.plus(staker.escrowedAmount);
+  }
+  staker.timestamp = event.block.timestamp;
+  staker.save();
 }
