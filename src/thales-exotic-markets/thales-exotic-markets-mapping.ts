2b1b94bebb67041b841ef07128c7af8e8c10a2c3
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-03-11 17:19:08 +0100

    Add disputes

--- src/thales-exotic-markets/thales-exotic-markets-mapping.ts
+++ src/thales-exotic-markets/thales-exotic-markets-mapping.ts
@@ -1,5 +1,7 @@
+import { BigInt } from '@graphprotocol/graph-ts';
 import { MarketCreated as MarketCreatedEvent } from '../../generated/ExoticPositionalMarketManager/ExoticPositionalMarketManager';
-import { Market } from '../../generated/schema';
+import { NewDispute as NewDisputeEvent } from '../../generated/ThalesOracleCouncil/ThalesOracleCouncil';
+import { Dispute, Market } from '../../generated/schema';
 
 export function handleMarketCreatedEvent(event: MarketCreatedEvent): void {
   let market = new Market(event.params.marketAddress.toHex());
@@ -13,6 +15,25 @@ export function handleMarketCreatedEvent(event: MarketCreatedEvent): void {
   market.isWithdrawalAllowed = event.params.withdrawalAllowed;
   market.positions = event.params.positionPhrases;
   market.tags = event.params.tags;
+  market.isTicketType = event.params.fixedTicketPrice.gt(BigInt.fromI32(0));
   market.isOpen = true;
+  market.numberOfDisputes = BigInt.fromI32(0);
+  market.numberOfOpenDisputes = BigInt.fromI32(0);
   market.save();
+}
+
+export function handleNewDisputeEvent(event: NewDisputeEvent): void {
+  let market = Market.load(event.params.market.toHex());
+  market.numberOfDisputes = market.numberOfDisputes.plus(BigInt.fromI32(1));
+  market.numberOfOpenDisputes = market.numberOfOpenDisputes.plus(BigInt.fromI32(1));
+  market.save();
+  let dipsuteNumber = market.numberOfDisputes.plus(BigInt.fromI32(1));
+  let dispute = new Dispute(event.params.market.toHex() + '-' + dipsuteNumber.toString());
+  dispute.timestamp = event.block.timestamp;
+  dispute.creationDate = event.block.timestamp;
+  dispute.market = event.params.market;
+  dispute.disputor = event.params.disputorAccount;
+  dispute.reasonForDispute = event.params.disputeString;
+  dispute.status = 'open';
+  dispute.save();
 }
