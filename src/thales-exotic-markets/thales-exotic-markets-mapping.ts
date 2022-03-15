ab2043a3337b2e90eef7ed37d6ef9c648f95c657
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-03-15 16:16:34 +0100

    Adjust disputes logic

--- src/thales-exotic-markets/thales-exotic-markets-mapping.ts
+++ src/thales-exotic-markets/thales-exotic-markets-mapping.ts
@@ -1,7 +1,10 @@
 import { BigInt } from '@graphprotocol/graph-ts';
 import { MarketCreated as MarketCreatedEvent } from '../../generated/ExoticPositionalMarketManager/ExoticPositionalMarketManager';
-import { NewDispute as NewDisputeEvent } from '../../generated/ThalesOracleCouncil/ThalesOracleCouncil';
-import { Dispute, Market } from '../../generated/schema';
+import {
+  NewDispute as NewDisputeEvent,
+  VotedAddedForDispute as VotedAddedForDisputeEvent,
+} from '../../generated/ThalesOracleCouncil/ThalesOracleCouncil';
+import { Dispute, DisputeVote, Market } from '../../generated/schema';
 
 export function handleMarketCreatedEvent(event: MarketCreatedEvent): void {
   let market = new Market(event.params.marketAddress.toHex());
@@ -27,13 +30,33 @@ export function handleNewDisputeEvent(event: NewDisputeEvent): void {
   market.numberOfDisputes = market.numberOfDisputes.plus(BigInt.fromI32(1));
   market.numberOfOpenDisputes = market.numberOfOpenDisputes.plus(BigInt.fromI32(1));
   market.save();
-  let dipsuteNumber = market.numberOfDisputes.plus(BigInt.fromI32(1));
-  let dispute = new Dispute(event.params.market.toHex() + '-' + dipsuteNumber.toString());
+  let disputeNumber = market.numberOfOpenDisputes;
+  let dispute = new Dispute(event.params.market.toHex() + '-' + market.numberOfOpenDisputes.toString());
   dispute.timestamp = event.block.timestamp;
   dispute.creationDate = event.block.timestamp;
+  dispute.disputeNumber = disputeNumber;
   dispute.market = event.params.market;
-  dispute.disputor = event.params.disputorAccount;
+  dispute.disputer = event.params.disputorAccount;
   dispute.reasonForDispute = event.params.disputeString;
-  dispute.status = 'open';
+  dispute.isInPositioningPhase = event.params.disputeInPositioningPhase;
   dispute.save();
+}
+
+export function handleVotedAddedForDisputeEvent(event: VotedAddedForDisputeEvent): void {
+  // let market = Market.load(event.params.market.toHex());
+  // market.numberOfDisputes = market.numberOfDisputes.plus(BigInt.fromI32(1));
+  // market.numberOfOpenDisputes = market.numberOfOpenDisputes.plus(BigInt.fromI32(1));
+  // market.save();
+  let voteId =
+    event.params.market.toHex() + '-' + event.params.disputeIndex.toString() + '-' + event.transaction.from.toHex();
+  let disputeVote = DisputeVote.load(voteId);
+  if (disputeVote === null) {
+    disputeVote = new DisputeVote(voteId);
+    disputeVote.market = event.params.market;
+    disputeVote.voter = event.params.voter;
+    disputeVote.dispute = event.params.disputeIndex;
+  }
+  disputeVote.timestamp = event.block.timestamp;
+  disputeVote.vote = event.params.disputeCodeVote;
+  disputeVote.save();
 }
