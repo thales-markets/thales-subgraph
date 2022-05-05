cd39c77a1484e2fd10bafd3f5ab850e589c5aeba
makiBaraba <markovic.ofsec@gmail.com>
2022-05-05 23:38:19 +0200

    AMM Mappings

--- src/amm-mapping.ts
+++ src/amm-mapping.ts
@@ -4,22 +4,22 @@ import { BoughtFromAmm, SoldToAMM } from '../generated/AMM/AMM';
 import { Trade } from '../generated/schema';
 
 export function handleBoughtFromAmmEvent(event: BoughtFromAmm): void {
-  let trade = new Trade(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
-  trade.transactionHash = event.transaction.hash;
-  trade.timestamp = event.block.timestamp;
-  trade.blockNumber = event.block.number;
-  trade.orderHash = event.transaction.hash;
-  trade.maker = event.address;
-  trade.taker = event.params.buyer;
-  trade.makerToken = event.params.asset;
-  trade.takerToken = event.params.susd;
-  trade.makerAmount = event.params.amount;
-  trade.takerAmount = event.params.sUSDPaid;
+    let trade = new Trade(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
+    trade.transactionHash = event.transaction.hash;
+    trade.timestamp = event.block.timestamp;
+    trade.blockNumber = event.block.number;
+    trade.orderHash = event.transaction.hash;
+    trade.maker = event.address;
+    trade.taker = event.params.buyer;
+    trade.makerToken = event.params.asset;
+    trade.takerToken = event.params.susd;
+    trade.makerAmount = event.params.amount;
+    trade.takerAmount = event.params.sUSDPaid;
 
-  trade.market = event.params.market;
-  trade.optionSide = BigInt.fromI32(event.params.position).equals(BigInt.fromI32(0)) ? 'long' : 'short';
-  trade.orderSide = 'buy';
-  trade.save();
+    trade.market = event.params.market;
+    trade.optionSide = BigInt.fromI32(event.params.position).equals(BigInt.fromI32(0)) ? 'long' : 'short';
+    trade.orderSide = 'buy';
+    trade.save();
 }
 
 export function handleSoldToAMMEvent(event: SoldToAMM): void {
