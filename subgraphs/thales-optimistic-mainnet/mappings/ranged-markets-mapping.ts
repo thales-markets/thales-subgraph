5930340f1717ac22ce18dab1430d7791e191060a
Gorshtak <sg31191@gmail.com>
2022-06-08 10:03:56 +0200

    added refferal to mainnet

--- subgraphs/thales-optimistic-mainnet/mappings/ranged-markets-mapping.ts
+++ subgraphs/thales-optimistic-mainnet/mappings/ranged-markets-mapping.ts
@@ -1,4 +1,9 @@
-import { RangedMarketCreated, BoughtFromAmm, SoldToAMM } from '../../../generated/RangedMarkets/RangedMarketsAMM';
+import {
+  RangedMarketCreated,
+  BoughtFromAmm,
+  SoldToAMM,
+  ReferrerPaid,
+} from '../../../generated/RangedMarkets/RangedMarketsAMM';
 
 import {
   Market,
@@ -7,6 +12,9 @@ import {
   OptionTransaction,
   RangedPosition,
   RangedPositionBalance,
+  ReferralTransfer,
+  Referrer,
+  ReferredTrader,
 } from '../../../generated/schema';
 import { RangedMarket as RangedMarketTemplate } from '../../../generated/templates';
 import { Transfer as TransferEvent } from '../../../generated/templates/RangedPosition/RangedPosition';
@@ -147,4 +155,48 @@ export function handleMarketResolved(event: Resolved): void {
   market.result = event.params.winningPosition;
   market.finalPrice = event.params.finalPrice;
   market.save();
+}
+
+export function handleReferrerPaid(event: ReferrerPaid): void {
+  let referralTx = new ReferralTransfer(event.transaction.hash.toHexString());
+  referralTx.amount = event.params.amount;
+  referralTx.refferer = event.params.refferer;
+  referralTx.trader = event.params.trader;
+  referralTx.timestamp = event.block.timestamp;
+  referralTx.volume = event.params.volume;
+
+  referralTx.save();
+
+  let referrer = Referrer.load(event.params.refferer.toHex());
+
+  if (referrer == null) {
+    referrer = new Referrer(event.params.refferer.toHex());
+    referrer.totalVolume = event.params.volume;
+    referrer.totalEarned = event.params.amount;
+    referrer.trades = BigInt.fromI32(1);
+    referrer.timestamp = event.block.timestamp;
+  } else {
+    referrer.totalVolume = referrer.totalVolume.plus(event.params.volume);
+    referrer.totalEarned = referrer.totalEarned.plus(event.params.amount);
+    referrer.trades = referrer.trades.plus(BigInt.fromI32(1));
+  }
+
+  referrer.save();
+
+  let referredTrader = ReferredTrader.load(event.params.trader.toHex());
+
+  if (referredTrader == null) {
+    referredTrader = new ReferredTrader(event.params.trader.toHex());
+    referredTrader.trades = BigInt.fromI32(1);
+    referredTrader.totalVolume = event.params.volume;
+    referredTrader.totalEarned = event.params.amount;
+    referredTrader.refferer = event.params.refferer.toHex();
+    referredTrader.timestamp = event.block.timestamp;
+  } else {
+    referredTrader.trades = referredTrader.trades.plus(BigInt.fromI32(1));
+    referredTrader.totalVolume = referredTrader.totalVolume.plus(event.params.volume);
+    referredTrader.totalEarned = referredTrader.totalEarned.plus(event.params.amount);
+  }
+
+  referredTrader.save();
 }
