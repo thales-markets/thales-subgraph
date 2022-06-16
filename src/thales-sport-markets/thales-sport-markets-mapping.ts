26ce0fccea460fd0ff1fa69ddac4b132328863c8
dmikic <dusan.mikic15@gmail.com>
2022-06-16 17:20:04 +0200

    Added new ABI which provides normalized odds on GameOddsAdded event; temporarily preserved the old ABI for historical test markets

--- src/thales-sport-markets/thales-sport-markets-mapping.ts
+++ src/thales-sport-markets/thales-sport-markets-mapping.ts
@@ -1,10 +1,11 @@
-import { SportMarket } from '../../generated/schema';
+import { SportMarket, SportMarketOddsHistory } from '../../generated/schema';
 import {
   CreateSportsMarket as CreateSportsMarketEvent,
-  GameOddsAdded as GameOddsAddedEvent,
+  GameOddsAdded as GameOddsAddedHistoricalEvent,
   ResolveSportsMarket as ResolveSportsMarketEvent,
   GameResolved as GameResolvedEvent,
-} from '../../generated/TheRundownConsumer/TheRundownConsumer';
+} from '../../generated/TheRundownConsumer_historical_test/TheRundownConsumer_historical_test';
+import { GameOddsAdded as GameOddsAddedEvent } from '../../generated/TheRundownConsumer/TheRundownConsumer';
 import { BigInt } from '@graphprotocol/graph-ts';
 
 export function handleCreateSportsMarketEvent(event: CreateSportsMarketEvent): void {
@@ -24,15 +25,46 @@ export function handleCreateSportsMarketEvent(event: CreateSportsMarketEvent): v
   market.awayOdds = BigInt.fromI32(event.params._game.awayOdds);
   market.drawOdds = BigInt.fromI32(event.params._game.drawOdds);
   market.save();
+
+  let marketHistory = new SportMarketOddsHistory(event.params._id.toHex());
+  marketHistory.timestamp = event.block.timestamp;
+  marketHistory.address = event.params._marketAddress;
+  marketHistory.maturityDate = event.params._game.startTime;
+  marketHistory.tags = event.params._tags;
+  marketHistory.isOpen = true;
+  marketHistory.isResolved = false;
+  marketHistory.finalResult = BigInt.fromI32(0);
+  marketHistory.poolSize = BigInt.fromI32(0);
+  marketHistory.numberOfParticipants = BigInt.fromI32(0);
+  marketHistory.homeTeam = event.params._game.homeTeam;
+  marketHistory.awayTeam = event.params._game.awayTeam;
+  marketHistory.homeOdds = [BigInt.fromI32(event.params._game.homeOdds)];
+  marketHistory.awayOdds = [BigInt.fromI32(event.params._game.awayOdds)];
+  marketHistory.drawOdds = [BigInt.fromI32(event.params._game.drawOdds)];
+  marketHistory.save();
 }
 
-export function handleGameOddsAddedEvent(event: GameOddsAddedEvent): void {
+export function handleGameOddsAddedHistoricalEvent(event: GameOddsAddedHistoricalEvent): void {
   let market = SportMarket.load(event.params._id.toHex());
   if (market !== null) {
     market.homeOdds = BigInt.fromI32(event.params._game.homeOdds);
     market.awayOdds = BigInt.fromI32(event.params._game.awayOdds);
     market.drawOdds = BigInt.fromI32(event.params._game.drawOdds);
     market.save();
+  }
+
+  let marketHistory = SportMarketOddsHistory.load(event.params._id.toHex());
+  if (marketHistory !== null) {
+    let homeOdds = marketHistory.homeOdds;
+    homeOdds.push(BigInt.fromI32(event.params._game.homeOdds));
+    let awayOdds = marketHistory.awayOdds;
+    awayOdds.push(BigInt.fromI32(event.params._game.awayOdds));
+    let drawOdds = marketHistory.drawOdds;
+    drawOdds.push(BigInt.fromI32(event.params._game.drawOdds));
+    marketHistory.homeOdds = homeOdds;
+    marketHistory.awayOdds = awayOdds;
+    marketHistory.drawOdds = drawOdds;
+    marketHistory.save();
   }
 }
 
@@ -52,5 +84,31 @@ export function handleGameResolvedEvent(event: GameResolvedEvent): void {
     market.homeScore = BigInt.fromI32(event.params._game.homeScore);
     market.awayScore = BigInt.fromI32(event.params._game.awayScore);
     market.save();
+  }
+}
+
+export function handleGameOddsAddedEvent(event: GameOddsAddedEvent): void {
+  let market = SportMarket.load(event.params._id.toHex());
+  if (market !== null) {
+    let normalizedOdds = event.params._normalizedOdds;
+    market.homeOdds = normalizedOdds[0];
+    market.awayOdds = normalizedOdds[1];
+    market.drawOdds = normalizedOdds[2];
+    market.save();
+  }
+
+  let marketHistory = SportMarketOddsHistory.load(event.params._id.toHex());
+  if (marketHistory !== null) {
+    let normalizedOdds = event.params._normalizedOdds;
+    let homeOdds = marketHistory.homeOdds;
+    homeOdds.push(normalizedOdds[0]);
+    let awayOdds = marketHistory.awayOdds;
+    awayOdds.push(normalizedOdds[1]);
+    let drawOdds = marketHistory.drawOdds;
+    drawOdds.push(normalizedOdds[2]);
+    marketHistory.homeOdds = homeOdds;
+    marketHistory.awayOdds = awayOdds;
+    marketHistory.drawOdds = drawOdds;
+    marketHistory.save();
   }
 }
