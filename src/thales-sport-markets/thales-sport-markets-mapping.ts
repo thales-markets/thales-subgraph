45d9ac1439c8f1002f05c5bc84f3968e3301102c
dmikic <dusan.mikic15@gmail.com>
2022-06-21 13:32:05 +0200

    Updated sports market subgraph with new TheRundownConsumer contract; removed old code for history preservation

--- src/thales-sport-markets/thales-sport-markets-mapping.ts
+++ src/thales-sport-markets/thales-sport-markets-mapping.ts
@@ -1,16 +1,14 @@
 import { SportMarket, SportMarketOddsHistory } from '../../generated/schema';
 import {
   CreateSportsMarket as CreateSportsMarketEvent,
-  GameOddsAdded as GameOddsAddedHistoricalEvent,
+  GameOddsAdded as GameOddsAddedEvent,
   ResolveSportsMarket as ResolveSportsMarketEvent,
   GameResolved as GameResolvedEvent,
-  TheRundownConsumer_historical_test,
-} from '../../generated/TheRundownConsumer_historical_test/TheRundownConsumer_historical_test';
-import { CreateSportsMarket as CreateSportsMarketNewEvent } from '../../generated/TheRundownConsumer_newCreateMarketEvent/TheRundownConsumer_newCreateMarketEvent';
-import { GameOddsAdded as GameOddsAddedEvent } from '../../generated/TheRundownConsumer/TheRundownConsumer';
+} from '../../generated/TheRundownConsumer/TheRundownConsumer';
 import { BigInt } from '@graphprotocol/graph-ts';
 
 export function handleCreateSportsMarketEvent(event: CreateSportsMarketEvent): void {
+  let normalizedOdds = event.params._normalizedOdds;
   let market = new SportMarket(event.params._id.toHex());
   market.timestamp = event.block.timestamp;
   market.address = event.params._marketAddress;
@@ -23,9 +21,9 @@ export function handleCreateSportsMarketEvent(event: CreateSportsMarketEvent): v
   market.numberOfParticipants = BigInt.fromI32(0);
   market.homeTeam = event.params._game.homeTeam;
   market.awayTeam = event.params._game.awayTeam;
-  market.homeOdds = BigInt.fromI32(event.params._game.homeOdds);
-  market.awayOdds = BigInt.fromI32(event.params._game.awayOdds);
-  market.drawOdds = BigInt.fromI32(event.params._game.drawOdds);
+  market.homeOdds = normalizedOdds[0];
+  market.awayOdds = normalizedOdds[1];
+  market.drawOdds = normalizedOdds[2];
   market.save();
 
   let marketHistory = new SportMarketOddsHistory(event.params._id.toHex());
@@ -40,52 +38,10 @@ export function handleCreateSportsMarketEvent(event: CreateSportsMarketEvent): v
   marketHistory.numberOfParticipants = BigInt.fromI32(0);
   marketHistory.homeTeam = event.params._game.homeTeam;
   marketHistory.awayTeam = event.params._game.awayTeam;
-  marketHistory.homeOdds = [BigInt.fromI32(event.params._game.homeOdds)];
-  marketHistory.awayOdds = [BigInt.fromI32(event.params._game.awayOdds)];
-  marketHistory.drawOdds = [BigInt.fromI32(event.params._game.drawOdds)];
+  market.homeOdds = normalizedOdds[0];
+  market.awayOdds = normalizedOdds[1];
+  market.drawOdds = normalizedOdds[2];
   marketHistory.save();
-}
-
-export function handleGameOddsAddedHistoricalEvent(event: GameOddsAddedHistoricalEvent): void {
-  let market = SportMarket.load(event.params._id.toHex());
-  if (market !== null) {
-    market.homeOdds = BigInt.fromI32(event.params._game.homeOdds);
-    market.awayOdds = BigInt.fromI32(event.params._game.awayOdds);
-    market.drawOdds = BigInt.fromI32(event.params._game.drawOdds);
-    market.save();
-  }
-
-  let marketHistory = SportMarketOddsHistory.load(event.params._id.toHex());
-  if (marketHistory !== null) {
-    if (
-      event.params._game.homeOdds != event.params._game.awayOdds &&
-      event.params._game.homeOdds != event.params._game.drawOdds
-    ) {
-      let theRundownConsumerContract = TheRundownConsumer_historical_test.bind(event.address);
-      let normalizedOdds = theRundownConsumerContract.getNormalizedOdds(event.params._id);
-      let homeOdds = marketHistory.homeOdds;
-      homeOdds.push(normalizedOdds[0]);
-      let awayOdds = marketHistory.awayOdds;
-      awayOdds.push(normalizedOdds[1]);
-      let drawOdds = marketHistory.drawOdds;
-      drawOdds.push(normalizedOdds[2]);
-      marketHistory.homeOdds = homeOdds;
-      marketHistory.awayOdds = awayOdds;
-      marketHistory.drawOdds = drawOdds;
-      marketHistory.save();
-    } else {
-      let homeOdds = marketHistory.homeOdds;
-      homeOdds.push(BigInt.fromI32(event.params._game.homeOdds));
-      let awayOdds = marketHistory.awayOdds;
-      awayOdds.push(BigInt.fromI32(event.params._game.awayOdds));
-      let drawOdds = marketHistory.drawOdds;
-      drawOdds.push(BigInt.fromI32(event.params._game.drawOdds));
-      marketHistory.homeOdds = homeOdds;
-      marketHistory.awayOdds = awayOdds;
-      marketHistory.drawOdds = drawOdds;
-      marketHistory.save();
-    }
-  }
 }
 
 export function handleResolveSportsMarketEvent(event: ResolveSportsMarketEvent): void {
@@ -131,41 +87,4 @@ export function handleGameOddsAddedEvent(event: GameOddsAddedEvent): void {
     marketHistory.drawOdds = drawOdds;
     marketHistory.save();
   }
-}
-
-export function handleNewCreateSportsMarketEvent(event: CreateSportsMarketNewEvent): void {
-  let normalizedOdds = event.params._normalizedOdds;
-  let market = new SportMarket(event.params._id.toHex());
-  market.timestamp = event.block.timestamp;
-  market.address = event.params._marketAddress;
-  market.maturityDate = event.params._game.startTime;
-  market.tags = event.params._tags;
-  market.isOpen = true;
-  market.isResolved = false;
-  market.finalResult = BigInt.fromI32(0);
-  market.poolSize = BigInt.fromI32(0);
-  market.numberOfParticipants = BigInt.fromI32(0);
-  market.homeTeam = event.params._game.homeTeam;
-  market.awayTeam = event.params._game.awayTeam;
-  market.homeOdds = normalizedOdds[0];
-  market.awayOdds = normalizedOdds[1];
-  market.drawOdds = normalizedOdds[2];
-  market.save();
-
-  let marketHistory = new SportMarketOddsHistory(event.params._id.toHex());
-  marketHistory.timestamp = event.block.timestamp;
-  marketHistory.address = event.params._marketAddress;
-  marketHistory.maturityDate = event.params._game.startTime;
-  marketHistory.tags = event.params._tags;
-  marketHistory.isOpen = true;
-  marketHistory.isResolved = false;
-  marketHistory.finalResult = BigInt.fromI32(0);
-  marketHistory.poolSize = BigInt.fromI32(0);
-  marketHistory.numberOfParticipants = BigInt.fromI32(0);
-  marketHistory.homeTeam = event.params._game.homeTeam;
-  marketHistory.awayTeam = event.params._game.awayTeam;
-  market.homeOdds = normalizedOdds[0];
-  market.awayOdds = normalizedOdds[1];
-  market.drawOdds = normalizedOdds[2];
-  marketHistory.save();
 }
