76eae3daef39ffa5758dabf02b31ebe56cef42b4
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-08-03 20:14:21 +0200

    Add support for referrals and ranged markets on Polygon

--- src/amm-mapping.ts
+++ src/amm-mapping.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-empty */
 import { BigInt } from '@graphprotocol/graph-ts';
-import { BoughtFromAmm, SoldToAMM } from '../generated/AMM/AMM';
-import { Trade } from '../generated/schema';
+import { BoughtFromAmm, SoldToAMM, ReferrerPaid } from '../generated/AMM/AMM';
+import { Trade, ReferralTransfer, ReferredTrader, Referrer } from '../generated/schema';
 
 export function handleBoughtFromAmmEvent(event: BoughtFromAmm): void {
   let trade = new Trade(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
@@ -39,4 +39,48 @@ export function handleSoldToAMMEvent(event: SoldToAMM): void {
   trade.optionSide = BigInt.fromI32(event.params.position).equals(BigInt.fromI32(0)) ? 'long' : 'short';
   trade.orderSide = 'sell';
   trade.save();
+}
+
+export function handleReferrerPaid(event: ReferrerPaid): void {
+  let referralTx = new ReferralTransfer(event.transaction.hash.toHexString());
+  referralTx.amount = event.params.amount;
+  referralTx.refferer = event.params.refferer;
+  referralTx.trader = event.params.trader;
+  referralTx.timestamp = event.block.timestamp;
+  referralTx.volume = event.params.volume;
+
+  referralTx.save();
+
+  let referrer = Referrer.load(event.params.refferer.toHex());
+
+  if (referrer == null) {
+    referrer = new Referrer(event.params.refferer.toHex());
+    referrer.totalVolume = event.params.volume;
+    referrer.totalEarned = event.params.amount;
+    referrer.trades = BigInt.fromI32(1);
+    referrer.timestamp = event.block.timestamp;
+  } else {
+    referrer.totalVolume = referrer.totalVolume.plus(event.params.volume);
+    referrer.totalEarned = referrer.totalEarned.plus(event.params.amount);
+    referrer.trades = referrer.trades.plus(BigInt.fromI32(1));
+  }
+
+  referrer.save();
+
+  let referredTrader = ReferredTrader.load(event.params.trader.toHex());
+
+  if (referredTrader == null) {
+    referredTrader = new ReferredTrader(event.params.trader.toHex());
+    referredTrader.trades = BigInt.fromI32(1);
+    referredTrader.totalVolume = event.params.volume;
+    referredTrader.totalEarned = event.params.amount;
+    referredTrader.refferer = event.params.refferer.toHex();
+    referredTrader.timestamp = event.block.timestamp;
+  } else {
+    referredTrader.trades = referredTrader.trades.plus(BigInt.fromI32(1));
+    referredTrader.totalVolume = referredTrader.totalVolume.plus(event.params.volume);
+    referredTrader.totalEarned = referredTrader.totalEarned.plus(event.params.amount);
+  }
+
+  referredTrader.save();
 }
--- src/ranged-markets-mapping.ts
+++ src/ranged-markets-mapping.ts
@@ -1,9 +1,29 @@
-import { RangedMarketCreated, BoughtFromAmm, SoldToAMM } from '../generated/RangedMarkets/RangedMarketsAMM';
-
-import { Market, Trade, RangedMarket, OptionTransaction } from '../generated/schema';
+import {
+  RangedMarketCreated,
+  BoughtFromAmm,
+  SoldToAMM,
+  ReferrerPaid,
+} from '../generated/RangedMarkets/RangedMarketsAMM';
+import {
+  Market,
+  Trade,
+  RangedMarket,
+  OptionTransaction,
+  RangedPosition,
+  RangedPositionBalance,
+  ReferralTransfer,
+  Referrer,
+  ReferredTrader,
+} from '../generated/schema';
 import { RangedMarket as RangedMarketTemplate } from '../generated/templates';
-
-import { Exercised, Mint, RangedMarket as RangedMarketContract } from '../generated/RangedMarkets/RangedMarket';
+import { Transfer as TransferEvent } from '../generated/templates/RangedPosition/RangedPosition';
+import {
+  Exercised,
+  Mint,
+  RangedMarket as RangedMarketContract,
+  Resolved,
+} from '../generated/RangedMarkets/RangedMarket';
+import { RangedPosition as RangedPositionContract } from '../generated/templates';
 import { BigInt } from '@graphprotocol/graph-ts';
 
 export function handleRangedMarket(event: RangedMarketCreated): void {
@@ -15,19 +35,59 @@ export function handleRangedMarket(event: RangedMarketCreated): void {
 
   let rangedMarketContract = RangedMarketContract.bind(event.params.market);
 
-  rangedMarket.timestamp = event.block.timestamp;
-  rangedMarket.currencyKey = leftMarket.currencyKey;
-  rangedMarket.maturityDate = leftMarket.maturityDate;
-  rangedMarket.expiryDate = leftMarket.expiryDate;
-  rangedMarket.leftPrice = leftMarket.strikePrice;
-  rangedMarket.rightPrice = rightMarket.strikePrice;
-  rangedMarket.inAddress = rangedMarketContract.positions().value0;
-  rangedMarket.outAddress = rangedMarketContract.positions().value1;
-  rangedMarket.isOpen = true;
-  rangedMarket.leftMarket = leftMarket.id;
-  rangedMarket.rightMarket = rightMarket.id;
+  if (leftMarket !== null && rightMarket !== null) {
+    rangedMarket.timestamp = event.block.timestamp;
+    rangedMarket.currencyKey = leftMarket.currencyKey;
+    rangedMarket.maturityDate = leftMarket.maturityDate;
+    rangedMarket.expiryDate = leftMarket.expiryDate;
+    rangedMarket.leftPrice = leftMarket.strikePrice;
+    rangedMarket.rightPrice = rightMarket.strikePrice;
+    rangedMarket.inAddress = rangedMarketContract.positions().value0;
+    rangedMarket.outAddress = rangedMarketContract.positions().value1;
+    rangedMarket.isOpen = true;
+    rangedMarket.leftMarket = leftMarket.id;
+    rangedMarket.rightMarket = rightMarket.id;
 
-  rangedMarket.save();
+    rangedMarket.save();
+  }
+
+  let inPosition = new RangedPosition(rangedMarket.inAddress.toHex());
+  inPosition.side = 'in';
+  inPosition.market = rangedMarket.id;
+  inPosition.save();
+
+  let outPosition = new RangedPosition(rangedMarket.outAddress.toHex());
+  outPosition.side = 'out';
+  outPosition.market = rangedMarket.id;
+  outPosition.save();
+
+  RangedPositionContract.create(rangedMarketContract.positions().value0);
+  RangedPositionContract.create(rangedMarketContract.positions().value1);
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let position = RangedPosition.load(event.address.toHex());
+  if (position !== null) {
+    let userBalanceFrom = RangedPositionBalance.load(event.address.toHex() + ' - ' + event.params.from.toHex());
+    if (userBalanceFrom === null) {
+      userBalanceFrom = new RangedPositionBalance(event.address.toHex() + ' - ' + event.params.from.toHex());
+      userBalanceFrom.account = event.params.from;
+      userBalanceFrom.amount = BigInt.fromI32(0);
+      userBalanceFrom.position = position.id;
+    }
+    userBalanceFrom.amount = userBalanceFrom.amount.minus(event.params.value);
+    userBalanceFrom.save();
+
+    let userBalanceTo = RangedPositionBalance.load(event.address.toHex() + ' - ' + event.params.to.toHex());
+    if (userBalanceTo === null) {
+      userBalanceTo = new RangedPositionBalance(event.address.toHex() + ' - ' + event.params.to.toHex());
+      userBalanceTo.account = event.params.to;
+      userBalanceTo.amount = BigInt.fromI32(0);
+      userBalanceTo.position = position.id;
+    }
+    userBalanceTo.amount = userBalanceTo.amount.plus(event.params.value);
+    userBalanceTo.save();
+  }
 }
 
 export function handleBoughtFromAmmEvent(event: BoughtFromAmm): void {
@@ -75,6 +135,7 @@ export function handleExercised(event: Exercised): void {
   optionTransactionEntity.account = event.params.exerciser;
   optionTransactionEntity.market = event.address;
   optionTransactionEntity.amount = event.params.amount;
+  optionTransactionEntity.side = event.params._position;
   optionTransactionEntity.save();
 }
 
@@ -89,4 +150,57 @@ export function handleMint(event: Mint): void {
   optionTransactionEntity.amount = event.params.amount;
   optionTransactionEntity.side = event.params._position;
   optionTransactionEntity.save();
+}
+
+export function handleMarketResolved(event: Resolved): void {
+  let market = RangedMarket.load(event.address.toHex());
+  if (market !== null) {
+    market.result = event.params.winningPosition;
+    market.finalPrice = event.params.finalPrice;
+    market.save();
+  }
+}
+
+export function handleReferrerPaid(event: ReferrerPaid): void {
+  let referralTx = new ReferralTransfer(event.transaction.hash.toHexString());
+  referralTx.amount = event.params.amount;
+  referralTx.refferer = event.params.refferer;
+  referralTx.trader = event.params.trader;
+  referralTx.timestamp = event.block.timestamp;
+  referralTx.volume = event.params.volume;
+
+  referralTx.save();
+
+  let referrer = Referrer.load(event.params.refferer.toHex());
+
+  if (referrer == null) {
+    referrer = new Referrer(event.params.refferer.toHex());
+    referrer.totalVolume = event.params.volume;
+    referrer.totalEarned = event.params.amount;
+    referrer.trades = BigInt.fromI32(1);
+    referrer.timestamp = event.block.timestamp;
+  } else {
+    referrer.totalVolume = referrer.totalVolume.plus(event.params.volume);
+    referrer.totalEarned = referrer.totalEarned.plus(event.params.amount);
+    referrer.trades = referrer.trades.plus(BigInt.fromI32(1));
+  }
+
+  referrer.save();
+
+  let referredTrader = ReferredTrader.load(event.params.trader.toHex());
+
+  if (referredTrader == null) {
+    referredTrader = new ReferredTrader(event.params.trader.toHex());
+    referredTrader.trades = BigInt.fromI32(1);
+    referredTrader.totalVolume = event.params.volume;
+    referredTrader.totalEarned = event.params.amount;
+    referredTrader.refferer = event.params.refferer.toHex();
+    referredTrader.timestamp = event.block.timestamp;
+  } else {
+    referredTrader.trades = referredTrader.trades.plus(BigInt.fromI32(1));
+    referredTrader.totalVolume = referredTrader.totalVolume.plus(event.params.volume);
+    referredTrader.totalEarned = referredTrader.totalEarned.plus(event.params.amount);
+  }
+
+  referredTrader.save();
 }
