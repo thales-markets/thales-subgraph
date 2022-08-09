ea769bf954be4266ae446e3edc0500925ae0b28a
Gorshtak <sg31191@gmail.com>
2022-08-09 15:27:25 +0200

    added whole market entity to subgraph

--- Overtime/src/amm-mapping.ts
+++ Overtime/src/amm-mapping.ts
@@ -1,60 +1,74 @@
 /* eslint-disable no-empty */
 import { BigInt } from '@graphprotocol/graph-ts';
 import { BoughtFromAmm, SoldToAMM } from '../generated/SportsAMM/SportsAMM';
-import { MarketTransaction, Position, PositionBalance } from '../generated/schema';
+import { MarketTransaction, Position, PositionBalance, SportMarket, MarketToGameId } from '../generated/schema';
 
 export function handleBoughtFromAmmEvent(event: BoughtFromAmm): void {
   let transaction = new MarketTransaction(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
 
-  transaction.hash = event.transaction.hash;
-  transaction.timestamp = event.block.timestamp;
-  transaction.blockNumber = event.block.number;
-  transaction.type = 'buy';
-  transaction.account = event.params.buyer;
-  transaction.amount = event.params.amount;
-  transaction.position = BigInt.fromI32(event.params.position);
-  transaction.market = event.params.market;
-  transaction.paid = event.params.sUSDPaid;
-  transaction.save();
+  let marketToGameId = MarketToGameId.load(event.params.market.toHex());
+  if (marketToGameId !== null) {
+    let market = SportMarket.load(marketToGameId.gameId.toHex());
+    if (market !== null) {
+      transaction.hash = event.transaction.hash;
+      transaction.timestamp = event.block.timestamp;
+      transaction.blockNumber = event.block.number;
+      transaction.type = 'buy';
+      transaction.account = event.params.buyer;
+      transaction.amount = event.params.amount;
+      transaction.position = BigInt.fromI32(event.params.position);
+      transaction.market = event.params.market;
+      transaction.paid = event.params.sUSDPaid;
+      transaction.wholeMarket = market.id;
+      transaction.save();
 
-  let position = Position.load(event.params.asset.toHex());
-  if (position !== null) {
-    let userBalanceFrom = PositionBalance.load(position.id + ' - ' + event.params.buyer.toHex());
-    if (userBalanceFrom === null) {
-      userBalanceFrom = new PositionBalance(position.id + ' - ' + event.params.buyer.toHex());
-      userBalanceFrom.account = event.params.buyer;
-      userBalanceFrom.amount = BigInt.fromI32(0);
-      userBalanceFrom.position = position.id;
+      let position = Position.load(event.params.asset.toHex());
+      if (position !== null) {
+        let userBalanceFrom = PositionBalance.load(position.id + ' - ' + event.params.buyer.toHex());
+        if (userBalanceFrom === null) {
+          userBalanceFrom = new PositionBalance(position.id + ' - ' + event.params.buyer.toHex());
+          userBalanceFrom.account = event.params.buyer;
+          userBalanceFrom.amount = BigInt.fromI32(0);
+          userBalanceFrom.position = position.id;
+        }
+        userBalanceFrom.amount = userBalanceFrom.amount.plus(event.params.amount);
+        userBalanceFrom.save();
+      }
     }
-    userBalanceFrom.amount = userBalanceFrom.amount.plus(event.params.amount);
-    userBalanceFrom.save();
   }
 }
 
 export function handleSoldToAMMEvent(event: SoldToAMM): void {
   let transaction = new MarketTransaction(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
 
-  transaction.hash = event.transaction.hash;
-  transaction.timestamp = event.block.timestamp;
-  transaction.blockNumber = event.block.number;
-  transaction.type = 'sell';
-  transaction.account = event.params.seller;
-  transaction.amount = event.params.amount;
-  transaction.position = BigInt.fromI32(event.params.position);
-  transaction.market = event.params.market;
-  transaction.paid = event.params.sUSDPaid;
-  transaction.save();
+  let marketToGameId = MarketToGameId.load(event.params.market.toHex());
+  if (marketToGameId !== null) {
+    let market = SportMarket.load(marketToGameId.gameId.toHex());
+    if (market !== null) {
+      transaction.hash = event.transaction.hash;
+      transaction.timestamp = event.block.timestamp;
+      transaction.blockNumber = event.block.number;
+      transaction.type = 'sell';
+      transaction.account = event.params.seller;
+      transaction.amount = event.params.amount;
+      transaction.position = BigInt.fromI32(event.params.position);
+      transaction.market = event.params.market;
+      transaction.paid = event.params.sUSDPaid;
+      transaction.wholeMarket = market.id;
+      transaction.save();
 
-  let position = Position.load(event.params.asset.toHex());
-  if (position !== null) {
-    let userBalanceFrom = PositionBalance.load(position.id + ' - ' + event.params.seller.toHex());
-    if (userBalanceFrom === null) {
-      userBalanceFrom = new PositionBalance(position.id + ' - ' + event.params.seller.toHex());
-      userBalanceFrom.account = event.params.seller;
-      userBalanceFrom.amount = BigInt.fromI32(0);
-      userBalanceFrom.position = position.id;
+      let position = Position.load(event.params.asset.toHex());
+      if (position !== null) {
+        let userBalanceFrom = PositionBalance.load(position.id + ' - ' + event.params.seller.toHex());
+        if (userBalanceFrom === null) {
+          userBalanceFrom = new PositionBalance(position.id + ' - ' + event.params.seller.toHex());
+          userBalanceFrom.account = event.params.seller;
+          userBalanceFrom.amount = BigInt.fromI32(0);
+          userBalanceFrom.position = position.id;
+        }
+        userBalanceFrom.amount = userBalanceFrom.amount.minus(event.params.amount);
+        userBalanceFrom.save();
+      }
     }
-    userBalanceFrom.amount = userBalanceFrom.amount.minus(event.params.amount);
-    userBalanceFrom.save();
   }
 }
