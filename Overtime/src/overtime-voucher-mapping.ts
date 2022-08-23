094e8e7e77a7dd33cd084c4958b00c3db086c2f3
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-08-23 12:56:22 +0200

    Add vouchers on Goerli

--- Overtime/src/overtime-voucher-mapping.ts
+++ Overtime/src/overtime-voucher-mapping.ts
@@ -2,9 +2,9 @@ import { Transfer as TransferEvent } from '../generated/OvertimeVoucher/Overtime
 import { OvertimeVoucher } from '../generated/schema';
 
 export function handleTransfer(event: TransferEvent): void {
-  let overtimeVoucher = OvertimeVoucher.load(event.params.tokenId.toHex());
+  let overtimeVoucher = OvertimeVoucher.load(event.params.tokenId.toString());
   if (overtimeVoucher === null) {
-    overtimeVoucher = new OvertimeVoucher(event.params.tokenId.toHex());
+    overtimeVoucher = new OvertimeVoucher(event.params.tokenId.toString());
   }
   overtimeVoucher.address = event.params.to;
   overtimeVoucher.save();
