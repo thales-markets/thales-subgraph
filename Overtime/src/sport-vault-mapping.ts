2cb7c87e12713b61074f7aeacdcd8efc4c540cb7
Vladan Poklopic <vladanpoklopic@gmail.com>
2022-11-02 14:54:34 +0100

    Update vaults logic

--- Overtime/src/sport-vault-mapping.ts
+++ Overtime/src/sport-vault-mapping.ts
@@ -1,22 +1,25 @@
 /* eslint-disable no-empty */
 import { BigInt } from '@graphprotocol/graph-ts';
 import { TradeExecuted, RoundClosed } from '../generated/SportVault/SportVault';
-import { SportMarket, VaultPnl, VaultTransaction } from '../generated/schema';
+import { MarketToGameId, SportMarket, VaultPnl, VaultTransaction } from '../generated/schema';
 
 export function handleVaultTrade(event: TradeExecuted): void {
   let transaction = new VaultTransaction(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
 
-  let market = SportMarket.load(event.params.market.toHex());
-  if (market !== null) {
-    transaction.hash = event.transaction.hash;
-    transaction.timestamp = event.block.timestamp;
-    transaction.blockNumber = event.block.number;
-    transaction.amount = event.params.amount;
-    transaction.position = BigInt.fromI32(event.params.position);
-    transaction.market = event.params.market;
-    transaction.paid = event.params.quote;
-    transaction.wholeMarket = market.id;
-    transaction.save();
+  let marketToGameId = MarketToGameId.load(event.params.market.toHex());
+  if (marketToGameId !== null) {
+    let market = SportMarket.load(marketToGameId.gameId.toHex());
+    if (market !== null) {
+      transaction.hash = event.transaction.hash;
+      transaction.timestamp = event.block.timestamp;
+      transaction.blockNumber = event.block.number;
+      transaction.amount = event.params.amount;
+      transaction.position = BigInt.fromI32(event.params.position);
+      transaction.market = event.params.market;
+      transaction.paid = event.params.quote;
+      transaction.wholeMarket = market.id;
+      transaction.save();
+    }
   }
 }
 
