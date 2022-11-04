c6483a5041fafaad3ef87041b19b24e85d0f45aa
Gorshtak <sg31191@gmail.com>
2022-11-04 12:44:33 +0100

    added zebro nft

---
+++ Overtime/src/zebro-mapping.ts
@@ -0,0 +1,12 @@
+import { Zebro } from "../generated/schema";
+import { Mint } from "../generated/ZebroNFT/ZebroNFT";
+
+
+export function handleMint(event: Mint): void {
+    const zebro = new Zebro(event.params._id.toHex());
+    zebro.owner = event.params._recipient;
+    zebro.country = event.params._country;
+    zebro.countryName = event.params._countryName;
+    zebro.url = event.params._url;
+    zebro.save();
+}
