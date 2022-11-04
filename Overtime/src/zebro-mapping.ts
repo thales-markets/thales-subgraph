72cb9ebfd6e6cfb57825482350bbe2065fbd70d0
Gorshtak <sg31191@gmail.com>
2022-11-04 13:11:15 +0100

    fixed zebro mapping

--- Overtime/src/zebro-mapping.ts
+++ Overtime/src/zebro-mapping.ts
@@ -3,8 +3,9 @@ import { Mint } from "../generated/ZebroNFT/ZebroNFT";
 
 
 export function handleMint(event: Mint): void {
-    const zebro = new Zebro(event.params._id.toHex());
+    const zebro = new Zebro(event.transaction.from.toHex() + event.params._id.toHex());
     zebro.owner = event.params._recipient;
+    zebro.tokenId = event.params._id;
     zebro.country = event.params._country;
     zebro.countryName = event.params._countryName;
     zebro.url = event.params._url;
