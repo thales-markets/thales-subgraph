dd19297faecb0b50c624b3c8dc3f480e62f5edd5
Gorshtak <sg31191@gmail.com>
2023-03-02 15:23:34 +0100

    added parlay vault transactions on goerli

--- Overtime/src/parlay-vault-mapping.ts
+++ Overtime/src/parlay-vault-mapping.ts
@@ -7,7 +7,7 @@ import {
   Deposited,
   WithdrawalRequested,
   Claimed,
-} from '../generated/DiscountVault/SportVault';
+} from '../generated/ParlayDiscountVault/ParlayVault';
 import {  Vault, VaultPnl, ParlayVaultTransaction, VaultUserTransaction, ParlayMarket } from '../generated/schema';
 
 export function handleVaultStarted(event: VaultStarted): void {
@@ -20,17 +20,20 @@ export function handleVaultStarted(event: VaultStarted): void {
 export function handleVaultTrade(event: TradeExecuted): void {
   let transaction = new ParlayVaultTransaction(event.transaction.hash.toHexString() + '-' + event.logIndex.toString());
 
-  let market = ParlayMarket.load(event.params.market.toHex());
-  if (market !== null) {
+
+
     transaction.vault = event.address;
     transaction.hash = event.transaction.hash;
     transaction.timestamp = event.block.timestamp;
     transaction.blockNumber = event.block.number;
-    transaction.amount = event.params.amount;
-    transaction.position = BigInt.fromI32(event.params.position);
-    transaction.market = event.params.market;
-    transaction.paid = event.params.quote;
-    transaction.wholeMarket = market.id;
+    transaction.market = event.params.parlayMarket;
+    transaction.paid = event.params.sUSDPaid;
+
+    let market = ParlayMarket.load(event.params.parlayMarket.toHex());
+    if(market !== null) {
+      transaction.wholeMarket = market.id;
+    }
+  
 
     let vault = Vault.load(event.address.toHex());
     if (vault !== null) {
@@ -38,7 +41,7 @@ export function handleVaultTrade(event: TradeExecuted): void {
     }
 
     transaction.save();
-  }
+  
 }
 
 export function handleRoundClosed(event: RoundClosed): void {
